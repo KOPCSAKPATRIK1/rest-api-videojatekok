d4184ddfc68d97cb6d6ed0c8b308286c9e7145d7
kopcsak patirk <kopcsakpatrik1@gmail.com>
2023-02-14 09:12:23 +0100

    class validation

--- videojatekok-backend/src/entities/videoGame.entity.ts
+++ videojatekok-backend/src/entities/videoGame.entity.ts
@@ -1,15 +1,18 @@
-import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { IsInt, MaxLength, Min } from 'class-validator';
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class VideoGame {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
+  @MaxLength(20, { message: 'A név maximum 15 karakter lehet' })
   name: string;
 
   @Column()
+  @IsInt({ message: 'Az árnak számnak kell lennie' })
+  @Min(1000, { message: 'Az árnak minimum 1000 forintnak kell lennie' })
   price: number;
 
   @Column()
